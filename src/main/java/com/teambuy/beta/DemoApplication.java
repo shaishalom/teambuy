343da9a41bd4245f2519884726cbf57c9fa7039f
Shai Shalom <Shai Shalom@SHALOM-WORK>
2020-02-11 18:36:12 +0200

    init version

---
+++ src/main/java/com/teambuy/beta/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.teambuy.beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("azarya and shai ha totachim shel cal");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
