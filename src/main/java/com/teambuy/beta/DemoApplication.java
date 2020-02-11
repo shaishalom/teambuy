0eb89fbe07ac9b2f50df5f1e8e36439a634f859e
shai shalom <shaishalom1@gmail.com>
2020-02-11 22:28:52 +0200

    put message at end

--- src/main/java/com/teambuy/beta/DemoApplication.java
+++ src/main/java/com/teambuy/beta/DemoApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("azarya and shai ha totachim shel cal");
+		
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("azarya and shai ha totachim shel cal");
 	}
 
 }
