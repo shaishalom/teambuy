9c3bab1f7cabe17fb993eb9d70269b0792bfedf2
shai1 <shai1@shalom1.lan>
2020-03-13 18:12:50 +0200

    insert release

---
+++ src/main/java/com/teambuy/demo/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.teambuy.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.teambuy.demo.model.EmployeeEntity;
+ 
+@Repository
+public interface EmployeeRepository
+        extends JpaRepository<EmployeeEntity, Long> {
+ 
+}
