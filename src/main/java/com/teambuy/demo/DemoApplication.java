9e40daccc3973b4619e853f8f43c3a1e791de86f
guy <guy@LAPTOP-GUY.lan>
2020-03-20 20:16:46 +0200

    put swagger

--- src/main/java/com/teambuy/demo/DemoApplication.java
+++ src/main/java/com/teambuy/demo/DemoApplication.java
@@ -2,12 +2,40 @@ package com.teambuy.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableWebMvc
+@EnableSwagger2
+public class DemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	//@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("SRQ068")
+				.description("This is a srq068 server ")
+				.license("Apache 2.0")
+				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+				.termsOfServiceUrl("")
+				.version("1.0.0")
+				.build();
+	}
+
 
 }
